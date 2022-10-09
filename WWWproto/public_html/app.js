72ba7a4446944738373cd8e440ca8f9151d8d02a
Xpired <86982241+XpiredCantHack@users.noreply.github.com>
2022-10-09 13:19:00 +0200

    Add files via upload

--- WWWproto/public_html/app.js
+++ WWWproto/public_html/app.js
@@ -6,20 +6,37 @@
 'use strict';
 
 const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+if (switcher) {
+    switcher.addEventListener('click', function() {
+        document.body.classList.toggle('light-theme');
+        document.body.classList.toggle('dark-theme');
+        
+        const className = document.body.className;
+        if(className === "light-theme") {
+            this.textContent = "Dark";
+        } else {
+            this.textContent = "Light";
+        }
     
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
-    
-    console.log('current class name: ' + className);
-});
+        console.log('current class name: ' + className);
+    });
+};
 
-
+var inCall = false;
+const callJoiner = document.querySelector('#userJoinLeaveCallBtn');
+if (callJoiner) {
+    callJoiner.addEventListener('click', function() {
+        if (inCall === false) {
+            inCall = true;
+            document.getElementById('userJoinLeaveCallBtn').innerText="Leave Call";
+            console.log('Joined Call!');
+        }
+        else if (inCall === true) {
+            inCall = false;
+            document.getElementById('userJoinLeaveCallBtn').innerText="Join Call";
+            console.log("Left Call!");
+        }
+    });
+};
 
 
